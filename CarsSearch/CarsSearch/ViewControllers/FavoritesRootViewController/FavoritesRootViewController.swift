6709026dfc19997519f3a02734c19d7c4fe242ac
Yahia <yahia@Yahias-MacBook-Pro.local>
2017-05-07 21:25:47 +0300

    [ Start ] Start of (**UI**) Introduce floating bar on the bottom of the favorite list.   [ Start ]
    [ Add ] FavoritesRootViewController which will contain the Floating Bar and the Favorites View Controller
    
    - Story Board :
    [ Remove ]  the segue between Navigation Controller and  Favorites View Controller
    [ Add  ] FavoritesRootViewController View
        - Add Calculate Button
        - Add TextFiled
        - Add Container View which Will contain Favorites View Controller
        - Add an Embed Segue between  ContainerView and Favorites View Controller  named “embedFavoritesVC”
        - Add an “ root view controller ” Segue between Navigation Controller and FavoritesRootViewController

---
+++ CarsSearch/CarsSearch/ViewControllers/FavoritesRootViewController/FavoritesRootViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FavoritesRootViewController.swift
+//  CarsSearch
+//
+//  Created by Yahia on 5/7/17.
+//  Copyright © 2017 Yahia. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class FavoritesRootViewController:UIViewController{
+        
+    // MARK:- Helper Methods
+    
+    // MARK:- IBActions Methods
+    func btnClaculate(sender: UIButton) {
+        
+    }
+}
